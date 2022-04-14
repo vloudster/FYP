1d6b5b67435e2c037aaee009f4ec063b80c7d6e2
vloudster <mauer.vladimir@web.de>
2022-04-14 16:42:21 +0100

    setting up settings and configuration

---
+++ backend/backend/backend/settings/local.py
@@ -0,0 +1,13 @@
+from .base import *
+from .base import env
+
+
+DEBUG = TRUE
+
+
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("DJANGO_SECRET_KEY",default='django-insecure-@-w+)f%=iptmy&^w76m5*)gcejab!sfu!ibk!fpkz&ysv668i(')
+
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
