f1748fe68462a79c7407feab52314ac532080160
vloudster <mauer.vladimir@web.de>
2022-04-14 14:26:10 +0100

    adding signup skeleton

---
+++ frontend/src/components/homepage/Homepage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+
+class Homepage extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>Homepage</h1>
+      </Container>
+    );
+  }
+}
+
+export default Homepage;
